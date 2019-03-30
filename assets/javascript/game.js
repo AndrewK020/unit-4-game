f3d292f6e93f549ccce5753bf11d7c479b13e341
Andrew Kelly <drew12396@gmail.com>
2019-03-30 17:58:35 -0500

    set up before combat

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,31 +6,148 @@ var anakin = $("#AniRow");
 var darthMaul = $("#MaulRow");
 var darthSidious = $("#SidRow");
 
+var ObiWan = {
+    name: "Obi Wan",
+    health: 120,
+    attackPower: 6,
+    attackPoints: 6,
+    counterAttack: 6,
+    element: $("#ObiRow")
+}
+
+var Anakin = {
+    name: "Anakin",
+    health: 100,
+    attackPower: 7,
+    attackPoints: 7,
+    counterAttack: 7,
+    element: $("#AniRow")
+}
+
+var DarthMaul = {
+    name: "Darth Maul",
+    health: 180,
+    attackPower: 4,
+    attackPoints: 4,
+    counterAttack: 4,
+    element: $("#MaulRow")
+}
+
+var DarthSidious = {
+    name: "Darth Sidious",
+    health: 150,
+    attackPower: 5,
+    attackPoints: 5,
+    counterAttack: 5,
+    element: $("#SidRow")
+}
+
+
+var isPlayer = false;
+var isEnemy = false;
+
+var player;
+var enemy;
+var playerScore = 0;
+var enemyScore = 0;
 
 $(document).ready(function() {
-//    original = $("#main").clone();
+    original = $("#main").clone();
 
     $(obiWan).on("click", function() {
-        console.log("obiwan");
-        startGame(this);
+        if (!isPlayer) {
+            
+            startGame(this, [anakin, darthMaul, darthSidious], ObiWan);
+        }
+        
     });
     $(anakin).on("click", function() {
-        console.log("anakin");
-        startGame(this);
+        if (!isPlayer) {
+            
+            startGame(this, [obiWan, darthMaul, darthSidious], Anakin);
+        }
     });
     $(darthMaul).on("click", function() {
-        console.log("Darth Maul");
-        startGame(this);
+       if (!isPlayer) {
+        
+        startGame(this, [anakin, obiWan, darthSidious], DarthMaul);
+       }
     });
     $(darthSidious).on("click", function() {
-        console.log("Darth Sidious");
-        startGame(this);
+        if (!isPlayer) {
+            
+            startGame(this, [anakin, darthMaul, obiWan], DarthSidious);
+        }
     });
 
+    $("#btn").on("click", function() {
+       
+        if(isPlayer && isEnemy) {
+            playGame();
+        }
+    })
 
 });
 
-function startGame(fighter) {
-    console.log(fighter);
-    $("#myCharacter").append(fighter);
-}
+    function startGame(fighter, enemies, object) {
+        isPlayer = true;
+
+        $("#myCharacter").append(fighter);
+        player = object;
+        playerScore = $(fighter).find("p.score").text();
+        $("fighter").off("click");
+
+        enemies.forEach(element => {
+            $(element).css("background-color", "red");
+            $(element).css("border-color", "black");
+            $("#enemies").append(element);
+        });
+
+        enemies.forEach(element => {
+            $(element).on("click", function() {
+                
+                if(!isEnemy) {
+                    $("#defender").append(element);
+
+                    enemy = (element).attr("id");
+                    enemyScore = $(element).find("p.score").text();
+
+                    $(element).css("background-color", "green");
+
+                    enemies.forEach(element => {
+                        $(element).off("click");
+                    });
+                    isEnemy = true;
+
+                    switch(enemy){
+                        case "MaulRow":
+                        enemy = DarthMaul;
+                        break;
+
+                        case "ObiRow":
+                        enemy = ObiWan;
+                        break;
+
+                        case "AniRow":
+                        enemy = Anakin;
+                        break;
+
+                        case "SidRow":
+                        enemy = DarthSidious;
+                        break;
+                    }
+                }
+            });
+        });
+        
+    }
+
+    function playGame() {
+        console.log(player);
+        console.log(enemy);
+    }
+
+    function resetGame() {
+        $("#main").replaceWith(original.clone());
+
+    }   
